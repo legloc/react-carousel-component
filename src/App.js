f1bda59501ace9157e74fee53670ede8ee81b52a
legloc <aliiev.akhtem@gmail.com>
2021-05-02 22:46:48 +0300

    Fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import Carousel from './components/Carousel'
 import './App.sass';
 
+import SlideImage1 from './images/Slide1.jpg'
+import SlideImage2 from './images/Slide2.jpg'
+import SlideImage3 from './images/Slide3.jpg'
+
+const slides = [{
+  id: 1,
+  title: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit',
+  desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus porro amet optio rem hic possimus deleniti in iste voluptatem! Neque consequatur consequuntur, incidunt soluta quia ea blanditiis hic voluptatibus quidem.',
+  image: SlideImage1
+}, {
+  id: 2,
+  title: 'Lorem ipsum dolor sit amet',
+  desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus porro amet optio rem hic possimus',
+  image: SlideImage2
+}, {
+  id: 3,
+  title: 'Lorem ipsum dolor sit amet consectetur',
+  desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus porro amet optio rem hic possimus deleniti in iste voluptatem!',
+  image: SlideImage3
+}]
+
 const App = () => (
   <div style={{ marginTop: 100, marginBottom: 100 }}>
-    <Carousel />
+    <Carousel slides={ slides } />
   </div>
 )
 
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,28 +2,7 @@ import { useState } from 'react'
 import Slide from './Slide'
 import Control from './Control'
 
-import SlideImage1 from '../images/Slide1.jpg'
-import SlideImage2 from '../images/Slide2.jpg'
-import SlideImage3 from '../images/Slide3.jpg'
-
-const slides = [{
-  id: 1,
-  title: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit',
-  desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus porro amet optio rem hic possimus deleniti in iste voluptatem! Neque consequatur consequuntur, incidunt soluta quia ea blanditiis hic voluptatibus quidem.',
-  image: SlideImage1
-}, {
-  id: 2,
-  title: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit',
-  desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus porro amet optio rem hic possimus deleniti in iste voluptatem! Neque consequatur consequuntur, incidunt soluta quia ea blanditiis hic voluptatibus quidem.',
-  image: SlideImage2
-}, {
-  id: 3,
-  title: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit',
-  desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus porro amet optio rem hic possimus deleniti in iste voluptatem! Neque consequatur consequuntur, incidunt soluta quia ea blanditiis hic voluptatibus quidem.',
-  image: SlideImage3
-}]
-
-const Carousel = () => {
+const Carousel = ({ slides }) => {
   const [step, setStep] = useState(0)
 
   return (
