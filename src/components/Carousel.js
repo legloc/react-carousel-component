798236514493f8c63cc906a60a39171ccbfaa2fd
legloc <aliiev.akhtem@gmail.com>
2021-05-03 17:02:02 +0300

    Created Hover animation

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -4,17 +4,24 @@ import Control from './Control'
 
 const Carousel = ({ slides }) => {
   const [step, setStep] = useState(0)
+  const [isItemHovered, setItemHovered] = useState(false)
 
   return (
     <div className="carousel">
       <div className="carousel-wrapper">
         <div className="carousel-track" style={{ transform: `translateX(${ -step * 100 }%)` }}>
           { slides.map(slide => (
-            <Slide key={ slide.id } className={ step === slide.id - 1 ? 'active' : 'inactive' } title={ slide.title } desc={ slide.desc } image={ slide.image } />
+            <Slide 
+              key={ slide.id } 
+              className={ step === slide.id - 1 ? 'active' : 'inactive' } 
+              title={ slide.title } 
+              desc={ slide.desc } 
+              image={ slide.image }
+              onHover={ e => setItemHovered(e) } />
           )) }
         </div>
       </div>
-      <div className="carousel-controls">
+      <div className={ `carousel-controls ${ isItemHovered ? 'hovered' : '' }` }>
         <Control direction="prev" onClick={ () => setStep(step - 1) } disabled={ step === 0 } />
         <Control direction="next" onClick={ () => setStep(step + 1) } disabled={ step === slides.length - 1 } />
       </div>
--- src/components/Slide.js
+++ src/components/Slide.js
@@ -1,17 +1,32 @@
-const Slide = ({ title, desc, image, className }) => (
-  <div className={ `carousel-item ${ className }` }>
-    <img className="item-img" src={ image } alt="Slide#1" />
-    <div className="item-content">
-      <h1 className="item-title">{ title }</h1>
-      <p className="item-desc">{ desc }</p>
-      <a className="item-link" href="/">
-        <span className="link-title">Shop now</span>
-        <svg className="link-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" display="block">
-          <path d="M4 12h16"/><path d="M13 5l7 7-7 7"/>
-        </svg>
-      </a>
-    </div>
-  </div>
-)
+import { useState } from 'react'
 
+const Slide = ({ title, desc, image, className, onHover }) => {  
+  const [isHovered, setHovered] = useState(false)
+  
+  const handleMouseEnter = () => {
+    setHovered(true)
+    onHover(true)
+  }
+
+  const handleMouseLeave = () => {
+    setHovered(false)
+    onHover(false)
+  }
+
+  return (
+    <div className={ `carousel-item ${ className } ${ isHovered ? 'hovered' : '' }` }>
+      <img className="item-img" src={ image } alt="Slide#1" />
+      <div className="item-content">
+        <h1 className="item-title">{ title }</h1>
+        <p className="item-desc">{ desc }</p>
+        <a className="item-link" href="/" onMouseEnter={ handleMouseEnter } onMouseLeave={ handleMouseLeave } >
+          <span className="link-title">Shop now</span>
+          <svg className="link-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" display="block">
+            <path d="M4 12h16"/><path d="M13 5l7 7-7 7"/>
+          </svg>
+        </a>
+      </div>
+    </div>
+  )
+}
 export default Slide
